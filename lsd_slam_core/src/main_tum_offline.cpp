d86952d345b2554fcff210883bfb2f6ff83b5d6b
W. Nicholas Greene <wng@csail.mit.edu>
2017-03-16 19:13:34 -0400

    Implement tum offline node.

--- lsd_slam_core/src/main_tum_offline.cpp
+++ lsd_slam_core/src/main_tum_offline.cpp
@@ -30,6 +30,8 @@
 #include <dirent.h>
 #include <algorithm>
 
+#include <ros/ros.h>
+
 #include "IOWrapper/ROS/ROSOutput3DWrapper.h"
 #include "IOWrapper/ROS/rosReconfigure.h"
 
@@ -55,11 +57,52 @@ void getParamOrFail(const ros::NodeHandle& nh, const std::string& name, T* val) 
 }
 
 using namespace lsd_slam;
-int main( int argc, char** argv ) {
+int main(int argc,  char** argv ) {
 	ros::init(argc, argv, "LSD_SLAM");
 
+  ros::NodeHandle nh;
+  ros::NodeHandle pnh("~");
 
-	dynamic_reconfigure::Server<lsd_slam_core::LSDParamsConfig> srv(ros::NodeHandle("~"));
+  /*==================== Get input params  ====================*/
+  std::string input_file;
+  getParamOrFail(pnh, "input_file", &input_file);
+
+  std::string calib_file;
+  getParamOrFail(pnh, "calib_file", &calib_file);
+
+  std::string input_frame_str;
+  getParamOrFail(pnh, "input_frame", &input_frame_str);
+
+  ros_sensor_streams::TUMRGBDOfflineStream::InputFrame input_frame;
+  if (input_frame_str == "RDF") {
+    input_frame = ros_sensor_streams::TUMRGBDOfflineStream::InputFrame::RDF;
+  } else if (input_frame_str == "FLU") {
+    input_frame = ros_sensor_streams::TUMRGBDOfflineStream::InputFrame::FLU;
+  } else if (input_frame_str == "FRD") {
+    input_frame = ros_sensor_streams::TUMRGBDOfflineStream::InputFrame::FRD;
+  } else if (input_frame_str == "RDF_IN_FLU") {
+    input_frame = ros_sensor_streams::TUMRGBDOfflineStream::InputFrame::RDF_IN_FLU;
+  } else if (input_frame_str == "RDF_IN_FRD") {
+    input_frame = ros_sensor_streams::TUMRGBDOfflineStream::InputFrame::RDF_IN_FRD;
+  } else {
+    ROS_ERROR("Unknown input frame!\n");
+    return 1;
+  }
+
+  double depth_scale_factor;
+  getParamOrFail(pnh, "depth_scale_factor", &depth_scale_factor);
+
+  std::string camera_frame_id;
+  getParamOrFail(pnh, "camera_frame_id", &camera_frame_id);
+
+  std::string camera_world_frame_id;
+  getParamOrFail(pnh, "camera_world_frame_id", &camera_world_frame_id);
+
+  double rate;
+  getParamOrFail(pnh, "rate", &rate);
+
+  /*==================== Set up dynamic reconfigure ====================*/
+  dynamic_reconfigure::Server<lsd_slam_core::LSDParamsConfig> srv(pnh);
 	srv.setCallback(dynConfCb);
 
 	dynamic_reconfigure::Server<lsd_slam_core::LSDDebugParamsConfig> srvDebug(ros::NodeHandle("~Debug"));
@@ -67,143 +110,60 @@ int main( int argc, char** argv ) {
 
 	packagePath = ros::package::getPath("lsd_slam_core")+"/";
 
-
-
-	// get camera calibration in form of an undistorter object.
-	// if no undistortion is required, the undistorter will just pass images through.
-	std::string calibFile;
-	Undistorter* undistorter = 0;
-	if(ros::param::get("~calib", calibFile))
-	{
-		 undistorter = Undistorter::getUndistorterForFile(calibFile.c_str());
-		 ros::param::del("~calib");
-	}
-
-	if(undistorter == 0)
-	{
-		printf("need camera calibration file! (set using _calib:=FILE)\n");
-		exit(0);
-	}
-
-	int w = undistorter->getOutputWidth();
-	int h = undistorter->getOutputHeight();
-
-	int w_inp = undistorter->getInputWidth();
-	int h_inp = undistorter->getInputHeight();
-
-	float fx = undistorter->getK().at<double>(0, 0);
-	float fy = undistorter->getK().at<double>(1, 1);
-	float cx = undistorter->getK().at<double>(2, 0);
-	float cy = undistorter->getK().at<double>(2, 1);
-	Sophus::Matrix3f K;
-	K << fx, 0.0, cx, 0.0, fy, cy, 0.0, 0.0, 1.0;
-
+  /*==================== Create input/output ====================*/
+  auto input = std::make_shared<ros_sensor_streams::
+                                 TUMRGBDOfflineStream>(nh,
+                                                       input_file,
+                                                       calib_file,
+                                                       "camera",
+                                                       camera_world_frame_id,
+                                                       camera_frame_id,
+                                                       input_frame,
+                                                       depth_scale_factor);
 
 	// make output wrapper. just set to zero if no output is required.
-	Output3DWrapper* outputWrapper = new ROSOutput3DWrapper(w,h);
+  Output3DWrapper* outputWrapper = new ROSOutput3DWrapper(input->width(),
+                                                          input->height());
 
 
 	// make slam system
-	SlamSystem* system = new SlamSystem(w, h, K, doSlam);
+  SlamSystem* system = new SlamSystem(input->width(), input->height(), input->K(), doSlam);
 	system->setVisualization(outputWrapper);
 
+  /*==================== Main loop ====================*/
+  int num_imgs = 0;
+  ros::Rate ros_rate(rate);
+  while (ros::ok() && !input->empty()) {
+    // Grab data.
+    uint32_t img_id;
+    double time;
+    cv::Mat3b rgb;
+    cv::Mat1f depth;
+    Eigen::Quaternionf q;
+    Eigen::Vector3f t;
+    input->get(&img_id, &time, &rgb, &depth, &q, &t);
+    ROS_INFO("Got image %i at time %f", img_id, time);
 
+    cv::Mat1b img_gray;
+    cv::cvtColor(rgb, img_gray, cv::COLOR_RGB2GRAY);
 
-	// open image files: first try to open as file.
-	std::string source;
-	std::vector<std::string> files;
-	if(!ros::param::get("~files", source))
-	{
-		printf("need source files! (set using _files:=FOLDER)\n");
-		exit(0);
-	}
-	ros::param::del("~files");
+    // Process image.
+    if (num_imgs == 0) {
+      system->randomInit(img_gray.data, time, img_id);
+    } else {
+      system->trackFrame(img_gray.data, img_id , false, time);
+    }
 
+    ros::spinOnce();
+    ros_rate.sleep();
+    num_imgs++;
+  }
 
-	if(getdir(source, files) >= 0)
-	{
-		printf("found %d image files in folder %s!\n", (int)files.size(), source.c_str());
-	}
-	else if(getFile(source, files) >= 0)
-	{
-		printf("found %d image files in file %s!\n", (int)files.size(), source.c_str());
-	}
-	else
-	{
-		printf("could not load file list! wrong path / file?\n");
-	}
+  // system->finalize();
 
-
-
-	// get HZ
-	double hz = 0;
-	if(!ros::param::get("~hz", hz))
-		hz = 0;
-	ros::param::del("~hz");
-
-
-
-	cv::Mat image = cv::Mat(h,w,CV_8U);
-	int runningIDX=0;
-	float fakeTimeStamp = 0;
-
-	ros::Rate r(hz);
-
-	for(unsigned int i=0;i<files.size();i++)
-	{
-		cv::Mat imageDist = cv::imread(files[i], CV_LOAD_IMAGE_GRAYSCALE);
-
-		if(imageDist.rows != h_inp || imageDist.cols != w_inp)
-		{
-			if(imageDist.rows * imageDist.cols == 0)
-				printf("failed to load image %s! skipping.\n", files[i].c_str());
-			else
-				printf("image %s has wrong dimensions - expecting %d x %d, found %d x %d. Skipping.\n",
-						files[i].c_str(),
-						w,h,imageDist.cols, imageDist.rows);
-			continue;
-		}
-		assert(imageDist.type() == CV_8U);
-
-		undistorter->undistort(imageDist, image);
-		assert(image.type() == CV_8U);
-
-		if(runningIDX == 0)
-			system->randomInit(image.data, fakeTimeStamp, runningIDX);
-		else
-			system->trackFrame(image.data, runningIDX ,hz == 0,fakeTimeStamp);
-		runningIDX++;
-		fakeTimeStamp+=0.03;
-
-		if(hz != 0)
-			r.sleep();
-
-		if(fullResetRequested)
-		{
-
-			printf("FULL RESET!\n");
-			delete system;
-
-			system = new SlamSystem(w, h, K, doSlam);
-			system->setVisualization(outputWrapper);
-
-			fullResetRequested = false;
-			runningIDX = 0;
-		}
-
-		ros::spinOnce();
-
-		if(!ros::ok())
-			break;
-	}
-
-
-	system->finalize();
-
-
+  ROS_INFO("Finished processing.");
 
 	delete system;
-	delete undistorter;
-	delete outputWrapper;
+  delete outputWrapper;
 	return 0;
 }
